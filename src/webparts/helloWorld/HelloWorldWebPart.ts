d9ec31d91dbf5934cda0c9e4a27f6a9be6f1855e
SanthoAnbros <santhoanbros@gmail.com>
2019-03-17 06:46:45 +0530

    Fixed Jquery date picker and auto complete issue

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -10,14 +10,21 @@ import styles from './HelloWorldWebPart.module.scss';
 import * as strings from 'HelloWorldWebPartStrings';
 import * as $ from 'jquery'
 
+//require('jquery-ui');
+//require('jqueryui');
+require("jquery-ui/ui/widgets/autocomplete");
+require("jquery-ui/ui/widgets/datepicker");
+
 export interface IHelloWorldWebPartProps {
   description: string;
 }
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
-
+ // <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+ //D:\Projects\CRUD\node_modules\jqueryui\jquery-ui.css
   public render(): void {
     this.domElement.innerHTML = `
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
       <div class="${ styles.helloWorld }">
         <div class="${ styles.container }">
           <div class="${ styles.row }">
@@ -29,6 +36,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
                 <span class="${ styles.label }">Learn more</span>
               </a>
               <input id="inp" type="text"/>
+              <input id="inpD" type="text"/>
               <button type="button" id="btnC">Click</button>
             </div>
           </div>
@@ -45,6 +53,19 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
        this.domElement.querySelector('#btnC').addEventListener('click', () => {
           this._GetListItemsNF();
        });
+
+       
+
+       var availableTutorials  =  [
+        "ActionScript",
+        "Bootstrap",
+        "C",
+        "C++"
+     ];
+     (<any>$( "#inpD" )).datepicker();
+     (<any>$( "#inp" )).autocomplete({
+      source: ['1101', '1202', '1303']
+     });
     }
     
     
